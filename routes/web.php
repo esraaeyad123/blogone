c87fe28ece1d19ff8a86a100bf5ec5f5c46ce1db
unknown <sosoeyad@hotmail.com>
2020-11-03 15:37:01 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+
+// Front End Routes
+Route::get('/', 'FrontEndController@home')->name('website');
+Route::get('/about', 'FrontEndController@about')->name('website.about');
+Route::get('/category/{slug}', 'FrontEndController@category')->name('website.category');
+Route::get('/tag/{slug}', 'FrontEndController@tag')->name('website.tag');
+Route::get('/contact', 'FrontEndController@contact')->name('website.contact');
+
+Route::post('/contact', 'FrontEndController@send_message')->name('website.contact');
+Route::get('/post/{slug}', 'FrontEndController@post')->name('website.post');
+
+
+
+
+
+
+
+Route::get('/admin', function () {
+    return view('layouts.admin');
+});
+Route::get('/test', function () {
+    return view('admin.dashboard.index');
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::get('/dashboard','DashboardController@index')->name('dashboard');
+
+    Route::resource('category', 'CategoryController');
+    Route::resource('tag', 'TagController');
+    Route::resource('post', 'PostController');
+    Route::resource('user', 'UserController');
+    Route::get('/profile', 'UserController@profile')->name('user.profile');
+    Route::post('/profile', 'UserController@profile_update')->name('user.profile.update');
+
+    // setting
+    Route::get('setting', 'SettingController@edit')->name('setting.index');
+    Route::post('setting', 'SettingController@update')->name('setting.update');
+
+    // Contact message
+    Route::get('/contact', 'ContactController@index')->name('contact.index');
+    Route::get('/contact/show/{id}', 'ContactController@show')->name('contact.show');
+    Route::delete('/contact/delete/{id}', 'ContactController@destroy')->name('contact.destroy');
+});
